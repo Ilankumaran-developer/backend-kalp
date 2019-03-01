4397fe602f46f229b1de95cfe8d4664278ac4034
ilanthedev <ilankumaran.1@partner.samsung.com>
2019-03-01 14:12:35 +0530

    refactoring

--- schema.js
+++ schema.js
@@ -1,24 +1,27 @@
 
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+require('mongoose-double')(mongoose);
 const Schema = mongoose.Schema;
-
+const SchemaTypes = mongoose.Schema.Types;
 
 const productSchema = new Schema({
     productname: String,
-    cost_price: Number,
+    cost_price: SchemaTypes.Double,
     description:String,
-    selling_price: Number,
+    selling_price: SchemaTypes.Double,
     date_created:Date,
-    profit:Number
+    profit:SchemaTypes.Double,
+    min_unit:SchemaTypes.Double,
+    price_per_gram:SchemaTypes.Double
 })
 
 const transSchema = new Schema({
     user: String,
     transaction:[],
-    service_tax: Number,
-    total:Number,
-    profit:Number,
-    grand_total:Number,
+    service_tax: SchemaTypes.Double,
+    total:SchemaTypes.Double,
+    profit:SchemaTypes.Double,
+    grand_total:SchemaTypes.Double,
     date_created:Date,
     product_ids:[]
    
